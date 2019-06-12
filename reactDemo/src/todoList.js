6e7d4608679d82bf0118f21c467ae799b3931ba8
liuxiaoru <liuxiaoru@17shanyuan.com>
2019-06-12 19:06:10 +0800

    test

--- reactDemo/src/todoList.js
+++ reactDemo/src/todoList.js
@@ -1,11 +1,20 @@
 import React,{Component,Fragment} from 'react';
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+        //组件状态
+        this.state = {
+            inputValue:'1111',
+            listArr:[]
+        };
+    }
+
     render(){
         return(
             //jsx语法外层要有一个包裹元素,用Fragment占位符，隐藏最外层标签
             <Fragment>
-                <div><input/><button>提交</button></div>
+                <div><input value={this.state.inputValue}/><button>提交</button></div>
                 <ul>
                     <li>香蕉</li>
                     <li>苹果</li>
