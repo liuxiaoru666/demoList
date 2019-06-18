0011e285c12e3a20947b56af55e6e4ffc5dec992
liuxiaoru <liuxiaoru@17shanyuan.com>
2019-06-18 15:54:36 +0800

    test

--- react-redux/src/store/reducer.js
+++ react-redux/src/store/reducer.js
@@ -1,19 +1,12 @@
 const defaultState={
     inputValue:'',
-    listArr:['dell']
+    listArr:['dell','hello']
 }
 export default (state=defaultState,action)=>{
     if(action.type==='change_input_value'){
         const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState;
-    }
-
-    if(action.type==='add_item'){
-        const newState = JSON.parse(JSON.stringify(state));
-        newState.listArr.push(action.value);
-        return newState;
-
     }
     return state;
 }
