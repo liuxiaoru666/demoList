8d1bb2c2ef543a311bd43e9a3eae9a0580c25f61
liuxiaoru <liuxiaoru@17shanyuan.com>
2019-06-27 11:21:47 +0800

    首页异步请求

--- reactproject/src/pages/home/store/actionCreators.js
+++ reactproject/src/pages/home/store/actionCreators.js
@@ -8,10 +8,30 @@ export const updataWriterList=(data)=>({
     totalPage:Math.ceil(data.writerList.length/5)
 })
 
+export const updataTopicList=(data)=>({
+    type:constants.UPDATA_TOPIC_LIST,
+    data:fromJS(data.topicList)
+})
+
+export const updataBoardList=(data)=>({
+    type:constants.UPDATA_BOARD_LIST,
+    data:fromJS(data.boardList)
+})
+
+export const updataHomeList=(data)=>({
+    type:constants.UPDATA_HOME_LIST,
+    data:fromJS(data.listArr)
+})
+
+
 export const getHomeData = ()=>{
     return (dispatch)=>{
         axios.get('./api/homeData.json').then((res)=>{
-            dispatch(updataWriterList(res.data.data))
+            let data = res.data.data;
+            dispatch(updataWriterList(data))
+            dispatch(updataTopicList(data))
+            dispatch(updataBoardList(data))
+            dispatch(updataHomeList(data))
         })
         
     }
